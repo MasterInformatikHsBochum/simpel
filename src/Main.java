814d8e07ba2d823be894bc7690be089d92ded85e
Marius Meisenzahl <mariusmeisenzahl@gmail.com>
2018-01-12 06:23:46 +0100

    Added example and updated src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,12 @@
+import java.io.IOException;
+
+import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+
 public class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) throws IOException {
+    	simpelLexer lexer = new simpelLexer(new ANTLRFileStream(args[0]));
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        simpelParser parser = new simpelParser(tokens);
+    }
 }
