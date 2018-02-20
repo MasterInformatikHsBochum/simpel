77ddddada6edf3771d0419923536e90792d7a476
Mario Groneick <Mario.groneick@googlemail.com>
2018-02-20 14:26:38 +0100

    paar sachen

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 import java.io.IOException;
+import java.util.Arrays;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import org.antlr.v4.gui.TreeViewer;
 import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
@@ -13,8 +18,21 @@ public class Main {
         CommonTokenStream tokens = new CommonTokenStream(lexer);
 
         simpelParser parser = new simpelParser(tokens);
-        ParseTree tree = parser.expression();
-
+        ParseTree tree = parser.decision();
+        
+        
+        JFrame frame = new JFrame("Antlr AST");
+        JPanel panel = new JPanel();
+        TreeViewer viewr = new TreeViewer(Arrays.asList(
+                parser.getRuleNames()),tree);
+        viewr.setScale(1.5);//scale a little
+        panel.add(viewr);
+        frame.add(panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(200,200);
+        frame.setVisible(true);        
+        
+        
         simpelGenerator generator = new simpelGenerator();
         String code = generator.generate(tree);
         
